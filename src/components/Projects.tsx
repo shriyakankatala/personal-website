c7c18b985057dd77762e666d508e377fd7b671d2
shriyakankatala <146775865+shriyakankatala@users.noreply.github.com>
2024-07-26 14:06:03 -0400

    media queries, added multiple links for project

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import './Sections.css';
 import Divider from '@mui/material/Divider';
 import Typography from '@mui/material/Typography';
@@ -8,16 +8,17 @@ interface ProjectsProps {
 }
 
 const Projects: React.FC<ProjectsProps> = ({ darkMode }) => {
+    const linkColor = darkMode ? '#F2D8FF' : '#581F73';
+
     return (
         <section className="section">
-            {/* <Divider flexItem sx={{fontSize: '1.6rem', fontWeight: 'bold'}}>PROJECTS</Divider> */}
             <Divider
                 className="divider-sub"
                 flexItem
                 sx={{
-                "&::before, &::after": {
-                    borderColor: darkMode ? '#F2D8FF' : '#581F73', // Adjust border color based on darkMode
-                },
+                    "&::before, &::after": {
+                        borderColor: linkColor,
+                    },
                 }}
             >
                 <Typography variant="h3" sx={{ fontSize: '1.6rem', fontWeight: 'bold', }} className="divider-sub">PROJECTS</Typography>
@@ -27,6 +28,10 @@ const Projects: React.FC<ProjectsProps> = ({ darkMode }) => {
                 <div>
                     <p><span className="title"><a href="https://closet-swap.vercel.app/" target="_blank" rel="noopener noreferrer">ClosetSwap</a></span><span className="title-sub"> - V1 Fall '23 Cohort</span></p>
                     <p className="description">Web platform marketplace to rent clothes and accessories quickly, reliably, and affordably from people on users' college campuses.</p>
+                    <p className="link-class">
+                        <a href="https://closet-swap.vercel.app/" target="_blank" rel="noopener noreferrer" className="link">Website</a><span className="link-divider"> | </span>
+                        <a href="https://github.com/diyamahaveer/ClosetSwap" target="_blank" rel="noopener noreferrer" className="link">GitHub</a>
+                    </p>
                 </div>
             </div>
             <div className="bullet">
@@ -34,6 +39,10 @@ const Projects: React.FC<ProjectsProps> = ({ darkMode }) => {
                 <div>
                     <p><span className="title"><a href="https://www.youtube.com/watch?v=VtMjJLMttso" target="_blank" rel="noopener noreferrer">Profit Peak</a></span><span className="title-sub"> - MHacks 16</span></p>
                     <p className="description">AI-driven solution for small business owners to determine their optimal price through a single picture of their product.</p>
+                    <p className="link-class">
+                        <a href="https://www.youtube.com/watch?v=VtMjJLMttso" target="_blank" rel="noopener noreferrer" className="link">Demo</a><span className="link-divider"> | </span>
+                        <a href="https://github.com/meghnar05/profitpeak" target="_blank" rel="noopener noreferrer" className="link">GitHub</a>
+                    </p>
                 </div>
             </div>
             <div className="bullet">
@@ -41,6 +50,10 @@ const Projects: React.FC<ProjectsProps> = ({ darkMode }) => {
                 <div>
                     <p><span className="title"><a href="https://www.figma.com/design/hDn3R28RqskULs5GWjs6dy/V1-Cohort-Tracker?node-id=0-1" target="_blank" rel="noopener noreferrer">Cohort Tracker</a></span><span className="title-sub"> - V1 Platform Team</span></p>
                     <p className="description">Centralized page for information on V1 Michigan's cohort's attendance, coffee chats, project progress, and assignment implementation with separate interfaces for pledges and members.</p>
+                    <p className="link-class">
+                        <a href="https://youtu.be/nLsHqjfqvs4?feature=shared" target="_blank" rel="noopener noreferrer" className="link">Demo</a><span className="link-divider"> | </span>
+                        <a href="https://www.figma.com/design/hDn3R28RqskULs5GWjs6dy/V1-Cohort-Tracker?node-id=0-1" target="_blank" rel="noopener noreferrer" className="link">Figma</a>
+                    </p>
                 </div>
             </div>
             <div margin-bottom='50px' className='extra-space'></div>
