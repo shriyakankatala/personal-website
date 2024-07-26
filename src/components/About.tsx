20916e624a9968aa42e4c263818432dee2693d11
shriyakankatala <146775865+shriyakankatala@users.noreply.github.com>
2024-07-26 00:47:38 -0400

    updated about section

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -10,7 +10,7 @@ const About: React.FC = () => {
                     <p>Hi, I'm Shriya!</p>
                     <p>I'm studying CSE and minoring in Business & Math at the University of Michigan.</p>
                     <p>I'm super passionate about using technology to improve social and mental well-being.</p>
-                    <p>Currently building a sheet music generator for any given mp3 file! - planning to add harmony generation as well in a future iteration.</p>
+                    <p>Currently trying to build a sheet music generator for any given mp3 file! - planning to add harmony generation as well in a future iteration.</p>
                     <p>Hobbies: singing, dancing, traveling, hiking!</p>
                     <p>Always down to meet new people and do spontaneous things :)</p>
                     <p>Let's chat: <a href="mailto:kankats@umich.edu" style={{ textDecoration: 'none', fontWeight: '600' }} className="hyperlink">kankats@umich.edu</a></p>
